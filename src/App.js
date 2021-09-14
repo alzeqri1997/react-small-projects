59acdc4949dc115b571e23a4218cc4caf75993ac
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-14 07:18:55 +0300

    add  main app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import './index.css';
+import Review from './Review';
 
 function App() {
   return (
-    
+    <section className="container">
+      <div className="title">
+        <h2>Reviews</h2>
+        <div className="underline"></div>
+      </div>
+      <Review />
+    </section>
   );
 }
 
