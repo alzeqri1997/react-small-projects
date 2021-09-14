86a0ec29551500cc35e776543c84a31b42bbf7bb
Ahmed Al-zeqri <ahmed1221moh@gmail.com>
2021-09-14 07:55:33 +0300

    add app component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
 
 function App() {
+  const [questions, setQuestions] = useState(data);
+
   return (
-    
+    <main>
+      <div className="container">
+        <h3>questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
   );
 }
 
